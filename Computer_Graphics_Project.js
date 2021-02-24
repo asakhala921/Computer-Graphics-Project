7e664e53ba9b598383b47cdbafb3fe2f5ee83147
kevinyao93 <kevinyao93@gmail.com>
2021-02-24 17:00:39 -0600

    Update empty project

---
+++ Computer_Graphics_Project.js
@@ -0,0 +1,30 @@
+import {defs, tiny} from '/examples/common.js';
+
+const {
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
+} = tiny;
+
+export class Computer_Graphics_Project extends Scene{
+    constructor(){
+        super();
+
+        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+    }
+
+    make_control_panel() {
+
+    }
+
+    display(context, program_state) {
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(this.initial_camera_location);
+        }
+
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, .1, 1000);
+        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+
+    }
+}
